f2b03ea7f544839d69aeaef3db30bcfa897c552d
aqib18 <mohammad.aqib@ylytic.com>
2023-02-14 15:33:25 +0530

    project completed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,21 @@
-import { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Box, Stack, Typography } from "@mui/material";
-import Sidebar from "./Sidebar";
+
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { Videos, Sidebar } from "./";
 
 const Feed = () => {
+	const [selectedCategory, setSelectedCategory] = useState("New");
+	const [videos, setVideos] = useState(null);
+
+	useEffect(() => {
+		setVideos(null);
+
+		fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) =>
+			setVideos(data.items)
+		);
+	}, [selectedCategory]);
+
 	return (
 		<Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
 			<Box
@@ -12,14 +25,17 @@ const Feed = () => {
 					px: { sx: 0, md: 2 },
 				}}
 			>
-				<Sidebar />
+				<Sidebar
+					selectedCategory={selectedCategory}
+					setSelectedCategory={setSelectedCategory}
+				/>
 
 				<Typography
 					className="copyright"
 					variant="body2"
 					sx={{ mt: 1.5, color: "#fff" }}
 				>
-					Copyright 2023 YouTube Clone
+					Copyright © 2022 JSM Media
 				</Typography>
 			</Box>
 
@@ -30,10 +46,10 @@ const Feed = () => {
 					mb={2}
 					sx={{ color: "white" }}
 				>
-					New <span style={{ color: "#f31503" }}>videos</span>
+					{selectedCategory} <span style={{ color: "#FC1503" }}>videos</span>
 				</Typography>
 
-        <Videos />
+				<Videos videos={videos} />
 			</Box>
 		</Stack>
 	);
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { Stack } from "@mui/material";
+
 import { categories } from "../utils/constants";
 
-const selectedCategory = "New";
-
-const Sidebar = () => (
+const Categories = ({ selectedCategory, setSelectedCategory }) => (
 	<Stack
 		direction="row"
 		sx={{
-			overflow: "auto",
+			overflowY: "auto",
 			height: { sx: "auto", md: "95%" },
 			flexDirection: { md: "column" },
 		}}
@@ -16,6 +15,7 @@ const Sidebar = () => (
 		{categories.map((category) => (
 			<button
 				className="category-btn"
+				onClick={() => setSelectedCategory(category.name)}
 				style={{
 					background: category.name === selectedCategory && "#FC1503",
 					color: "white",
@@ -30,10 +30,14 @@ const Sidebar = () => (
 				>
 					{category.icon}
 				</span>
-				<span style={{opacity : category.name === selectedCategory ? "1" : "0.8" }}>{category.name}</span>
+				<span
+					style={{ opacity: category.name === selectedCategory ? "1" : "0.8" }}
+				>
+					{category.name}
+				</span>
 			</button>
 		))}
 	</Stack>
 );
 
-export default Sidebar;
+export default Categories;
